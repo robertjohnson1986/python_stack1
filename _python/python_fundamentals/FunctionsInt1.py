ef864971cab438f6d3b06214ba39c7213bddfa68
robertjohnson1986 <83429757+robertjohnson1986@users.noreply.github.com>
2021-06-03 16:50:45 -0500

    Update FunctionsInt1.py

--- _python/python_fundamentals/FunctionsInt1.py
+++ _python/python_fundamentals/FunctionsInt1.py
@@ -14,11 +14,11 @@
 # between those 2 values.
 import random
 def randInt(min=0, max=0):
-    if max == 0:
-        max = 100
-    num = random.random() * max
-    num = round(num)
-    return num
+        if max == 0:
+            max = 100
+        num = random.uniform(min, max) 
+        num = round(num)
+        return num
 # print(randInt())         # should print a random integer between 0 to 100
 # print(randInt(max=50))   # should print a random integer between 0 to 50
 # print(randInt(min=50,max=100))   # should print a random integer between 50 to 100
