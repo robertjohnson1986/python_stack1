c2d2ae13c5b132292cc3f09e8d994b4d9f4e822f
robertjohnson1986 <83429757+robertjohnson1986@users.noreply.github.com>
2021-06-03 17:03:28 -0500

    Update FunctionsInt1.py

--- _python/python_fundamentals/FunctionsInt1.py
+++ _python/python_fundamentals/FunctionsInt1.py
@@ -14,11 +14,20 @@
 # between those 2 values.
 import random
 def randInt(min=0, max=0):
+        if min < 0:
+            return "min cannot be less than 0"
+        if max < 0:
+            return "max cannot be less than 0"
+        if min > max:
+            return "min cannot be greater than max"
         if max == 0:
             max = 100
         num = random.uniform(min, max) 
         num = round(num)
         return num
+# print(randInt(min=50, max=45)) #should print 'min cannot be greater than max'
+# print(randInt(min=-5)) #should print 'min cannot be less than zero'
+# print(randInt(max=-5)) #should print 'max cannot be less than zero'
 # print(randInt())         # should print a random integer between 0 to 100
 # print(randInt(max=50))   # should print a random integer between 0 to 50
 # print(randInt(min=50,max=100))   # should print a random integer between 50 to 100
