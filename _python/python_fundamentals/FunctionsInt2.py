a79d45f0361cf9c42a7265d479f54c3ed75858e1
robertjohnson1986 <83429757+robertjohnson1986@users.noreply.github.com>
2021-06-04 17:51:15 -0500

    Update FunctionsInt2.py

--- _python/python_fundamentals/FunctionsInt2.py
+++ _python/python_fundamentals/FunctionsInt2.py
@@ -50,3 +50,74 @@ def iterateDictionary(some_list):
 iterateDictionary(students)
 
 #3 - Third Challenge
+
+# Create a function iterateDictionary2
+# (key_name, some_list) that, given a 
+# list of dictionaries and a key name, 
+# the function prints the value stored 
+# in that key for each dictionary. 
+# For example, iterateDictionary2
+# ('first_name', students) should output:
+
+# Michael
+# John
+# Mark
+# KB
+
+# And iterateDictionary2('last_name', students) 
+# should output:
+
+# Jordan
+# Rosales
+# Guillen
+# Tonel
+
+students = [
+         {'first_name':  'Michael', 'last_name' : 'Jordan'},
+         {'first_name' : 'John', 'last_name' : 'Rosales'},
+         {'first_name' : 'Mark', 'last_name' : 'Guillen'},
+         {'first_name' : 'KB', 'last_name' : 'Tonel'}
+    ]
+def iterateDictionary2(key_name, some_list):
+    for idx in some_list:
+        print(idx[key_name])
+        
+        
+
+iterateDictionary2("last_name", students)
+
+#4 - Fourth Challenge
+# Create a function printInfo(some_dict) that given
+# a dictionary whose values are all lists, prints 
+# the name of each key along with the size of its 
+# list, and then prints the associated values 
+# within each key's list. For example:
+
+dojo = {
+   'locations': ['San Jose', 'Seattle', 'Dallas', 'Chicago', 'Tulsa', 'DC', 'Burbank'],
+   'instructors': ['Michael', 'Amy', 'Eduardo', 'Josh', 'Graham', 'Patrick', 'Minh', 'Devon']
+}
+def printInfo(some_dict):
+    for key in some_dict:
+        print(len(some_dict))
+        print(key.upper())
+printInfo(dojo)
+# output:
+# 7 LOCATIONS
+# San Jose
+# Seattle
+# Dallas
+# Chicago
+# Tulsa
+# DC
+# Burbank
+    
+# 8 INSTRUCTORS
+# Michael
+# Amy
+# Eduardo
+# Josh
+# Graham
+# Patrick
+# Minh
+# Devon  
