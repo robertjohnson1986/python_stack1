fe53e57670038e6943faa4d3cc86548f9f328d81
robertjohnson1986 <83429757+robertjohnson1986@users.noreply.github.com>
2021-06-05 15:01:08 -0500

    Create user.py

---
+++ _python/python_fundamentals/user.py
@@ -0,0 +1,58 @@
+# If you've been following along, you're going to utilize the 
+# User class we've been discussing for this assignment.
+
+# For this assignment, we'll add some functionality to the User class:
+
+# make_withdrawal(self, amount) - have this method decrease the user's 
+# balance by the amount specified
+# display_user_balance(self) - have this method print the user's name 
+# and account balance to the terminal
+# eg. "User: Guido van Rossum, Balance: $150
+# BONUS: transfer_money(self, other_user, amount) - have this method 
+# decrease the user's balance by the amount and add that amount to 
+# other other_user's balance
+# Create a file with the User class, including the __init__ and 
+# make_deposit methods  Add a make_withdrawal method to the User class
+# Add a display_user_balance method to the User class  Create 3 instances
+# of the User class  Have the first user make 3 deposits and 1 withdrawal
+# and then display their balance  Have the second user make 2 deposits 
+# and 2 withdrawals and then display their balance  Have the third user 
+# make 1 deposits and 3 withdrawals and then display their balance  
+# BONUS: Add a transfer_money method; have the first user transfer money 
+# to the third user and then print both users' balances
+class User:        # here's what we have so far
+    def __init__(self, name, email):
+        self.name = name
+        self.email = email
+        self.account_balance = 0
+    # adding the deposit method
+    def make_deposit(self, amount):    # takes an argument that is the amount of the deposit
+        self.account_balance += amount    # the specific user's account increases by the amount of the value received
+    def make_withdrawl(self, amount): #takes an argument that is the amount of the withdrawl
+        self.account_balance -= amount
+    def display_user_balance(self):
+        print("User:",self.name,", Balance: ", self.account_balance)
+guido = User("Guido van Rossum", "guido@python.com")
+monty = User("Monty Python", "monty@python.com")
+robert = User("Robert Johnson", "kuyperhouse@gmail.com")
+print(guido.name)    # output: Guido van Rossum
+print(monty.name)    # output: Monty Python
+print(robert.name)
+guido.make_deposit(100)
+guido.make_deposit(200)
+guido.make_deposit(100)
+robert.make_deposit(300)
+robert.make_deposit(6000)
+monty.make_deposit(50)
+guido.make_withdrawl(1)
+robert.make_withdrawl(1)
+robert.make_withdrawl(2.33)
+monty.make_withdrawl(1)
+monty.make_withdrawl(56.22)
+monty.make_withdrawl(28.69)
+monty.display_user_balance()
+guido.display_user_balance()
+robert.display_user_balance()
+print(guido.account_balance)    # output: 300
+print(monty.account_balance)    # output: 50
+print(robert.account_balance)
