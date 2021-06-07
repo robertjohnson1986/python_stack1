7bf18beea5309e2f4e681209248bb782375c3636
robertjohnson1986 <83429757+robertjohnson1986@users.noreply.github.com>
2021-06-07 15:23:40 -0500

    Update Association.py

--- _python/OOP/Association.py
+++ _python/OOP/Association.py
@@ -7,7 +7,7 @@ class BankAccount:
         self.balance += amount 
         return self
 
-    def withdraw(self, amount):
+    def withdrawl(self, amount):
         # check the balance and see if it's greater than the amount 
         if self.balance >= amount:
             self.balance -= amount 
@@ -25,14 +25,6 @@ class BankAccount:
             self.balance = self.balance + self.balance * self.int_rate
         return self
 
-
-ba1 = BankAccount(balance=100)
-ba2 = BankAccount(.01, 500)
-
-ba1.deposit(100).deposit(50).deposit(50).withdraw(100).yield_interest().display_account_info()
-ba2.deposit(100).deposit(100).withdraw(50).withdraw(50).withdraw(50).withdraw(50).yield_interest().display_account_info()
-
-
 class User:        # here's what we have so far
     def __init__(self, name, email):
         self.name = name
@@ -40,17 +32,17 @@ class User:        # here's what we have so far
         self.account = BankAccount(int_rate=0.02, balance=0)    # added this line
     # adding the deposit method
     def make_deposit(self, amount):    # takes an argument that is the amount of the deposit
-        self.account.deposit(100)        # we can call the BankAccount instance's methods
+        self.account.deposit(amount)        # we can call the BankAccount instance's methods
         print(self.account.balance)        # or access its attributes
         return self
 
     def make_withdrawal(self, amount):
-        self.account.withdrawl(100)        # we can call the BankAccount instance's methods
+        self.account.withdrawl(amount)        # we can call the BankAccount instance's methods
         print(self.account.balance)        # or access its attributes
         return self
     
     def display_user_balance(self):
-        print(f"User: {self.name}, Balance: ${self.account.balance}")
+        self.account.display_account_info()
         return self   
     
     def transfer_money(self, other_user, amount):
@@ -62,12 +54,9 @@ class User:        # here's what we have so far
         return self
 
 
-todd = User("Todd", "todd@todd.com")
-jane = User("Jane", "jane@jane.com")
-john = User("John", "john@john.com")
+robert = User("Robert", "rjenglish@gmail.com")
 
-todd.make_deposit(100).make_deposit(100).make_withdrawal(50).display_user_balance()
-
-jane.make_deposit(50).make_deposit(1000).make_withdrawal(500).make_withdrawal(100).display_user_balance()
-
-john.make_deposit(100).make_withdrawal(25).make_withdrawal(25).make_withdrawal(25).display_user_balance()
+robert.make_deposit(50).make_deposit(100).make_withdrawal(50).display_user_balance()
+robert.account.yield_interest()
+robert.display_user_balance()
+robert.account.display_account_info()
