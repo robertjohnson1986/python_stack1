17e6f230cf8bc7a96ccd688f06728771034748ac
robertjohnson1986 <83429757+robertjohnson1986@users.noreply.github.com>
2021-06-20 22:30:59 -0500

    More Django

--- django/django_fundamentals/dojo_survey/survey_app/views.py
+++ django/django_fundamentals/dojo_survey/survey_app/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
+def index(request):
+    return render(request, 'form.html')
+
+def process(request):
+    if request.method == 'POST':
+        context = {
+            'name': request.POST['name'],
+            'lang': request.POST['location'],
+            'loc': request.POST['language']
+        }
+        return render(request, 'result.html', context)
+    return render(request, 'result.html')
 
 # Create your views here.
