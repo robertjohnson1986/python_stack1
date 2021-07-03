bde7c17394b4045dc61204d425cbc65a14c590a5
robertjohnson1986 <83429757+robertjohnson1986@users.noreply.github.com>
2021-07-03 07:08:39 -0500

    More Django

--- django/django_fundamentals/BJ/BJapp/views.py
+++ django/django_fundamentals/BJ/BJapp/views.py
@@ -4,7 +4,22 @@ def index(request):
 
 def result(request):
     context = {
-    	"name": request.POST["name"]
+    	"why": request.POST["why"],
+        "title": request.POST["title"],
+        "obvious": request.POST["obvious"],
+        "important": request.POST["important"],
+        "info": request.POST["info"],
+        "support1": request.POST["support1"],
+        "case1": request.POST["case1"],
+        "support2": request.POST["support2"],
+        "case2": request.POST["case2"],
+        "support3": request.POST["support3"],
+        "case3": request.POST["case3"],
+        "well": request.POST["well"],
+        "restate": request.POST["restate"],
+        "summarize": request.POST["summarize"],
+        "added": request.POST["added"],
+        "slip": request.POST["slip"]
     }
     return render(request, "next.html", context)
     
