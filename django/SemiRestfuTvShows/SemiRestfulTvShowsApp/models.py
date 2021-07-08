9d6bde1877d13abe9e4c488d21cf926d2ca8a126
robertjohnson1986 <83429757+robertjohnson1986@users.noreply.github.com>
2021-07-08 15:16:19 -0500

    More Django

--- django/SemiRestfuTvShows/SemiRestfulTvShowsApp/models.py
+++ django/SemiRestfuTvShows/SemiRestfulTvShowsApp/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Show(models.Model):
     title = models.CharField(max_length=255)
     network = models.CharField(max_length=45)
-    release_date = models.IntegerField()
+    release_date = models.DateTimeField()
     description = models.CharField(max_length=255)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
